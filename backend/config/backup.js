5a9408d95e47b48cc6894452c6519eedafc940f1
Juan Pablo Arango <juanpabloarango.iearm@gmail.com>
2024-10-07 11:47:30 -0500

    Backup

---
+++ backend/config/backup.js
@@ -0,0 +1,17 @@
+const {exec} = require('child_process');
+require('dotenv').config();
+
+exports.backDatabase = async () => {
+    const dbName = process.env.BD;
+    const outputPath = './backup';
+
+    const command = `mongodump --uri "${process.env.MONGO_URI}" --out ${outputPath} --gzip`;
+
+    await exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error en el respaldo: ${error.message}`);
+            return;
+        }
+        console.log(`Respaldo completado con éxito ${stdout}`);
+    });
+}
